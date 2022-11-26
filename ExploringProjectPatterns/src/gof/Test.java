8bdfdbf97e04bd631c98c529808498de53be48ae
Wania Medeiros <wrmpratico@outlook.com>
2022-11-26 13:18:34 -0300

    add test

--- ExploringProjectPatterns/src/gof/Test.java
+++ ExploringProjectPatterns/src/gof/Test.java
@@ -1,4 +1,50 @@
 package gof;
+import gof.facade.Facade;
+import gof.singleton.SingletonEager;
+import gof.singleton.SingletonLazy;
+import gof.singleton.SingletonLazyHolder;
+import gof.strategy.*;
 
 public class Test {
+    public static void main(String[] args) {
+
+        // Singleton teste
+
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+
+        SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+        lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+
+        // Strategy teste
+
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+        robo.setComportamento(defensivo);
+        robo.mover();
+        robo.setComportamento(agressivo);
+        robo.mover();
+        robo.mover();
+        robo.mover();
+
+        // Facade teste
+
+        Facade facade = new Facade();
+        facade.migrarCliente("Venilton", "14801788");
+    }
 }
